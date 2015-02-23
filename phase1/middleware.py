152b279903f0186c8cfd6101edaa2c1c848cb029
Felix D <flx.descoteaux@gmail.com>
2015-02-22 22:24:39 -0500

    refactored middleware again...

--- phase1/middleware.py
+++ phase1/middleware.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 from django.http import Http404
 import re
+from .user_flow import phase1_redirect
 
 
 class AllauthOverrideMiddleware():
@@ -10,15 +11,10 @@ class AllauthOverrideMiddleware():
     def __init__(self):
         # allauth urls
         self.url_social = re.compile("^/accounts/.*$")
-        self.url_social_login = re.compile("^/accounts/login/.*$")
 
     def process_request(self, request):
 
-        # IF ALLAUTH LOGIN URL IS REQUESTED, REDIRECT TO ROOT
-        if self.url_social_login.match(request.path):
-            return redirect("/")
-        
         # AND CAN ONLY POST TO ALLAUTH URLS
         if request.method == "GET" and\
            self.url_social.match(request.path):
-            raise Http404("Allauth urls are not accessible with GET request")
+            return redirect("/")
