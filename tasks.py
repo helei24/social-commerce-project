c2448536878edba2e2b51fe1a221134d1a54e89c
Felix D <flx.descoteaux@gmail.com>
2015-03-23 18:37:54 -0400

    test

--- tasks.py
+++ tasks.py
@@ -2,9 +2,15 @@ from invoke import run, task
 
 
 @task
-def collectstatic():
-        run("python manage.py collectstatic")
+def deploy():
+    run("git add -A .")
+    print("what is your commit message")
+    msg = input('>')
+    run("git commit -am '{}'".format(msg))
+    run("git push origin master")
+    run("git push webfaction master")
+
 
 @task
-def bootstrap():
-        collectstatic()
+def collectstatic():
+    run("python manage.py collectstatic")
