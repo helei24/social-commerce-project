91eda3fa95621b8c8055c84bf7bf054868124000
Felix D <flx.descoteaux@gmail.com>
2015-03-24 20:42:56 -0400

    updated fabfile

--- tasks.py
+++ tasks.py
@@ -2,23 +2,20 @@ from invoke import run, task
 
 
 @task
-def deploy():
-    run("git add -A .")
-    print("what is your commit message")
-    msg = input('>')
-    run("git commit -am '{}'".format(msg))
-    run("git push webfaction master")
-
-
-@task
 def git():
-    run("git add -A .")
-    print("what is your commit message")
-    msg = input('>')
-    run("git commit -am '{}'".format(msg))
-    run("git push origin master")
-
+    run("git reset --hard HEAD")
+    run("git pull")
 
 @task
 def collectstatic():
-    run("python manage.py collectstatic")
+    run("python3.4 manage.py collectstatic")
+
+@task
+def restartserver():
+    run("../apache2/bin/restart")
+
+@task
+def pull():
+    git()
+    collectstatic()
+    restartserver()
