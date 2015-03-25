4463acbd5c4c3f4a426610ba9a41b73ef67514ae
Felix D <flx.descoteaux@gmail.com>
2015-03-25 15:50:40 -0400

    .

--- src/js/phase1/review_app/app.jsx
+++ src/js/phase1/review_app/app.jsx
@@ -5,11 +5,17 @@ var init = function init(data){
    var counter = 0; 
     function preload(sources) {
         var imagesTemp = [];
+        var imagesTempLg = [];
         for (var i = 0, l=sources.length; i < l; i++) {
             imagesTemp[i] = new Image();
+            imagesTempLg[i] = new Image();
 	    imagesTemp[i].src = sources[i].sm_image_path;
+	    imagesTempLg[i].src = sources[i].image_path;
             imagesTemp[i].onload = function(){
-                console.log("image loaded");
+                console.log("small image loaded");
+            }
+            imagesTempLg[i].onload = function(){
+                console.log("Large image loaded");
             }
         }
     }
