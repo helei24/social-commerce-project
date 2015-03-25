b91a7a5d32bd015c6c9232a79a2cabc7682adbac
Felix D <flx.descoteaux@gmail.com>
2015-03-25 19:23:14 -0400

    removed image preoloading

--- build/js/dev/phase1/review-app-bundle.js
+++ build/js/dev/phase1/review-app-bundle.js
@@ -33475,28 +33475,11 @@ var ReviewApp = require('./components/ReviewApp.react.jsx');
 //Called in the django template
 var init = function init(data){
    var counter = 0; 
-    function preload(sources) {
-        var imagesTemp = [];
-        var imagesTempLg = [];
-        for (var i = 0, l=sources.length; i < l; i++) {
-            imagesTemp[i] = new Image();
-            imagesTempLg[i] = new Image();
-	    imagesTemp[i].src = sources[i].sm_image_path;
-	    imagesTempLg[i].src = sources[i].image_path;
-            imagesTemp[i].onload = function(){
-                console.log("small image loaded");
-            }
-            imagesTempLg[i].onload = function(){
-                console.log("Large image loaded");
-            }
-        }
-    }
     //Rendering of root component
         React.render(
             React.createElement(ReviewApp, data),
             document.getElementById('reviewapp')
         );
-    preload(data.products);
 }
 
 module.exports = init;
