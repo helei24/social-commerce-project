90f5322e7379a1cf3420516309cea1140e5a6c7c
Felix D <flx.descoteaux@gmail.com>
2015-03-25 14:36:32 -0400

    preloading everything

--- build/js/dev/phase1/review-app-bundle.js
+++ build/js/dev/phase1/review-app-bundle.js
@@ -33475,20 +33475,23 @@ var ReviewApp = require('./components/ReviewApp.react.jsx');
 //Called in the django template
 var init = function init(data){
    var counter = 0; 
-    function preload(sources) {
-        var imagesTemp = [];
-        for (var i = 0, l=sources.length; i < l; i++) {
-            imagesTemp[i] = new Image();
-	    imagesTemp[i].src = sources[i].sm_image_path;
-        }
-    }
-    preload(data.products);
 
     //Rendering of root component
     React.render(
         React.createElement(ReviewApp, data),
         document.getElementById('reviewapp')
     );
+    function preload(sources) {
+        var imagesTemp = [];
+        var imagesTempLg = [];
+        for (var i = 0, l=sources.length; i < l; i++) {
+            imagesTemp[i] = new Image();
+	    imagesTemp[i].src = sources[i].sm_image_path;
+            imagesTempLg[i] = new Image();
+	    imagesTempLg[i].src = sources[i].image_path;
+        }
+    }
+    preload(data.products);
 }
 
 module.exports = init;
