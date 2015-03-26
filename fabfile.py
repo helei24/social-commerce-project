aee3912e790418d86f8945970893064cedb1c48d
Felix D <flx.descoteaux@gmail.com>
2015-03-25 21:38:22 -0400

    fabfile working!

--- fabfile.py
+++ fabfile.py
@@ -1,25 +1,23 @@
-from fabric.api import *
-
+from fabric.api import env, run, cd, local
 
 env.hosts = ['socialcomm@socialcomm.webfactional.com']
 
 
-def git_commit():
+def commit():
     local("git add -A .")
     msg = raw_input("Commit message: ")
     local("git commit -am '%s'" % msg)
     local("git push")
 
 
-def pull():
-    run("cd /home/socialcomm/webapps/hec/social_commerce_project/")
-    run("git reset --hard HEAD")
-    run("git pull")
-    run("python3.4 manage.py collectstatic --noinput")
-    run("../apache2/bin/restart")
+def server_pull():
+    with cd("/home/socialcomm/webapps/hec/social_commerce_project"):
+        run("git reset --hard HEAD")
+        run("git pull")
+        run("python3.4 manage.py collectstatic --noinput")
+        run("../apache2/bin/restart", pty=False)
 
 
 def deploy():
-    git_commit()
-    pull()
-
+    commit()
+    server_pull()
